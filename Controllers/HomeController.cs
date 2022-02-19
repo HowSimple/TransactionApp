8a68678ae4b4cccd7138e86fdcfe1e601b09ba11
Caleb Vaughn <43933950+HowSimple@users.noreply.github.com>
2022-02-19 14:45:36 -0600

    Testing if application can access database details

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,20 +6,31 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Configuration;
 
 namespace Commerce_TransactionApp.Controllers
 {
+   
     public class HomeController : Controller
-    {
+    { 
+        String connectionString;
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            var key = "testkey";
+           // connectionString = ConfigurationManager.ConnectionStrings[key]
+            //    .ConnectionString;
+            //ViewBag.connection = connectionString;
+          
+            
         }
+     
 
         public IActionResult Index()
         {
+            ViewBag.test = "test string";
             return View();
         }
 
@@ -27,6 +38,7 @@ namespace Commerce_TransactionApp.Controllers
         {
             return View();
         }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- Program.cs
+++ Program.cs
@@ -6,16 +6,31 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Configuration;
+
+
 
 namespace Commerce_TransactionApp
 {
     public class Program
-    {
+    {   
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+           
 
+            CreateHostBuilder(args).Build().Run();
+
+
+        }
+        public String GetConnectionString()
+        {
+            var key = "testkey";
+
+            string value = ConfigurationManager.ConnectionStrings[key]
+                .ConnectionString;
+            Console.WriteLine(value);
+            return key;
+        }
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
