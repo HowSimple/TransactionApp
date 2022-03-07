62245095b0813a603763ab024631b9d0749b2294
Marcus Garity <60191486+MarcusGarity@users.noreply.github.com>
2022-03-07 09:15:21 -0600

    Update StoredProcedures.sql

--- SQL/StoredProcedures.sql
+++ SQL/StoredProcedures.sql
@@ -1,17 +1,23 @@
+USE CommerceTransactionDB;
+
+
 --------------------------------------------------------------------------------------
 -- Stored Procedures
 
 --Login Procedure:
     --Inputs: userName, password
     --Output: userID
+    
+DROP PROCEDURE IF EXISTS LoginProcedure;
 
+GO
 CREATE PROCEDURE LoginProcedure(@username AS VARCHAR(10), @password AS VARCHAR(15))
 AS
-BEGIN
 	SELECT userId
 	FROM Users
 	WHERE userName = @username AND password = @password;
-END;
+GO
+
 
 --- execute procedure
 EXECUTE LoginProcedure
@@ -24,15 +30,17 @@ EXECUTE LoginProcedure
     --Input: userID
     --Output: table All transactions related to UserID accounts. order by date
 
+DROP PROCEDURE IF EXISTS TransactionSummaryProcedure;
+GO
 CREATE PROCEDURE TransactionSummaryProcedure(@userID AS INT)
 AS
-BEGIN
 	SELECT *
 	FROM UserTransactions
 	INNER JOIN Account
 	ON Account.userID = @userID
 	WHERE Account.accountNumber = UserTransactions.accountNumber;
-END;
+GO
+
 
 --- execute procedure
 EXECUTE TransactionSummaryProcedure
@@ -43,17 +51,58 @@ EXECUTE TransactionSummaryProcedure
 --Notification Procedure:
 	--Input: userID
 	--Output: table ALL notifications related to userID accounts
-
+DROP PROCEDURE IF EXISTS NotificationProcedure;
+GO
 CREATE PROCEDURE NotificationProcedure(@userId AS INT)
 AS
-BEGIN
 	SELECT *
 	FROM Notifications
 	INNER JOIN Account
 	ON Account.userID = @userID
 	WHERE Account.accountNumber = Notifications.accountNumber;
-END;
+GO
+
 
 --- execute procedure
 EXECUTE NotificationProcedure
 	@userID = 123;
+
+DROP PROCEDURE IF EXISTS TransactionProcedure;
+GO
+CREATE PROCEDURE TransactionProcedure(@userID AS INT, @transactionAmount AS FLOAT, @transactionLocation AS VARCHAR(2),@transactionType as VARCHAR(2), @processingDate as Date, @transactionDescription as VARCHAR(15))
+AS
+	BEGIN TRANSACTION 
+		 INSERT INTO UserTransactions (transactionType,processingDate,description,locations,amount,accountNumber)
+		 VALUES (@transactionType, @processingDate, @transactionDescription, @transactionLocation, @transactionAmount, 
+				(SELECT accountNumber FROM Account INNER JOIN Users On Account.userID = @userID WHERE Account.userID = Users.userId));
+	
+	
+		 IF (@transactionType = 'CR')
+			 UPDATE Balance
+			 SET amount = amount + @transactionAmount
+			 Where Balance.accountNumber = (SELECT accountNumber FROM Account INNER JOIN Users On Account.userID = @userID WHERE Account.userID = Users.userId);
+		 ELSE
+			UPDATE Balance
+			 SET amount = amount - @transactionAmount
+			 Where Balance.accountNumber = (SELECT accountNumber FROM Account INNER JOIN Users On Account.userID = @userID WHERE Account.userID = Users.userId);
+	COMMIT;
+	
+GO
+
+Select *
+FROM Users
+
+--- execute procedure
+EXECUTE TransactionProcedure
+          @userID = 123,
+		  @transactionAmount = 200,
+		  @transactionLocation = "MO",
+		  @transactionType = "DR",
+		  @processingDate = '2020-03-07',
+		  @transactionDescription = "Starbucks";
+
+SELECT * 
+FROM UserTransactions;
+
+SELECT *
+FROM Balance
