84530ce46711ef0eaac7b99e7f0e619afaba8645
Marcus Garity <60191486+MarcusGarity@users.noreply.github.com>
2022-03-08 02:37:11 -0600

    Add files via upload
    500 + withdraw Trigger Complete, Testing Triggers, and Data Entry for Current Trigger Notifications.

---
+++ SQL/Data Entry.sql
@@ -0,0 +1,7 @@
+
+
+INSERT INTO UserNotification (description) VALUES('Withdraw greater Than $500.00');
+INSERT INTO UserNotification (description) VALUES('Out of State Trasaction');
+INSERT INTO UserNotification (description) VALUES('Low Balance');
+
+
---
+++ SQL/Triggers.sql
@@ -0,0 +1,22 @@
+
+
+
+USE CommerceTransactionDB;
+
+
+-----------------------------------------------------------
+------------Triggers
+------------UserTransaction_500_Insert
+------------If user has greater than or equal to 500 notification set up a notification will appear.
+
+DROP TRIGGER IF EXISTS UserTransactions_500_Insert;
+GO
+CREATE TRIGGER UserTransactions_500_INSERT ON UserTransactions
+AFTER INSERT
+AS
+	SET NOCOUNT ON;
+	IF 1 = ISNULL((SELECT hasNotification FROM HasNotification Inner Join (SELECT Users.userId FROM Users Inner Join Account On Account.accountNumber = (SELECT accountNumber from inserted)) as far on far.userId = HasNotification.userID),0)
+		IF (SELECT amount FROM inserted) >= 500.00
+			INSERT INTO Notifications (description,accountNumber)
+			VALUES (CONCAT('The amount of ',(SELECT amount FROM inserted),' was withdrawn from account #',(SELECT accountNumber FROM inserted)), (SELECT accountNumber FROM inserted))	
+GO
