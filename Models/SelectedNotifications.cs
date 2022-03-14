3ace0995f80e0d0b3796efdb9e347872cba788d9
Caleb Vaughn <43933950+HowSimple@users.noreply.github.com>
2022-03-14 11:55:25 -0500

    Added Notification page: just displays all accounts notifications

---
+++ Models/SelectedNotifications.cs
@@ -0,0 +1,9 @@
+﻿namespace Commerce_TransactionApp.Models
+{
+    public class SelectedNotifications
+    {
+        public bool outOfState { get; set; }
+        public bool lowBalance { get; set; }
+        public bool largeWithdraw { get; set; }
+    }
+}
