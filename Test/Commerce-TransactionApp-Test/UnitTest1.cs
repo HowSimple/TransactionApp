0e1e2ad32419fbc1ff75424b37956b20d70aa61e
Job Dick <74458676+FakingIrish@users.noreply.github.com>
2022-04-26 16:19:12 -0500

    Beginning Testing
    coverlet.settings.xml - format coverlet report, open with Excel, simply at 15% coverage, needs more in-depth testing

--- Test/Commerce-TransactionApp-Test/UnitTest1.cs
+++ Test/Commerce-TransactionApp-Test/UnitTest1.cs
@@ -5,49 +5,137 @@ using System.Text;
 using Commerce_TransactionApp;
 using Microsoft.AspNetCore.Mvc;
 using Commerce_TransactionApp.Controllers;
+using Moq;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 
 namespace Commerce_TransactionApp_Test
 {
+    // coverlet test ran, export to .xml with formatting in coverlet.settings
+    // dotnet test --collect:"XPlat Code Coverage" --settings:coverlet.settings
+
     public class UnitTest1
     {
-       /* [Fact]
+        [Fact]
         public void Test1()
         {
+
+            //var logger = new Mock<ILogger<HomeController>>();
+
+            //var configuration = new Mock<IConfiguration>();
             //Arrange
-            HomeController controller = new HomeController();
+            HomeController controller = new HomeController(null, null);
 
 
 
 
             //Act
-            ViewResult result = controller.Index() as ViewResult;
+            ViewResult result = controller.Privacy() as ViewResult;
 
             //Assert
             Assert.NotNull(result);
         }
 
-    }
 
-    public class UnitTest2
-    {
+
+
         [Fact]
         public void Test2()
         {
-            
-                //Arrange
-                HomeController controller = new HomeController();
 
-                //Act
+            //Arrange
+            HomeController controller = new HomeController(null, null);
 
-                ViewResult result = controller.Privacy() as ViewResult;
+            //Act
+
+            ViewResult result = controller.Index() as ViewResult;
 
 
-                //Assert
-                Assert.NotNull(result);
-            
-        }*/
+            //Assert
+            Assert.NotNull(result);
+        }
 
+        /*[Fact]
+        public void Test3()
+        {
+
+            //Arrange
+            HomeController controller = new HomeController(null, null);
+
+            //Act
+
+            ViewResult result = controller.Register() as ViewResult;
+
+
+            //Assert
+            Assert.NotNull(result);
+        }
+
+
+        [Fact]
+        public void Test4()
+        {
+
+            //Arrange
+            HomeController controller = new HomeController(null, null);
+
+            //Act
+
+            ViewResult result = controller.Summary() as ViewResult;
+
+
+            //Assert
+            Assert.NotNull(result);
+        }
+
+
+        [Fact]
+        public void Test5()
+        {
+
+            //Arrange
+            HomeController controller = new HomeController(null, null);
+
+            //Act
+
+            ViewResult result = controller.Notifications() as ViewResult;
+
+
+            //Assert
+            Assert.NotNull(result);
+        }
+
+
+        //[Fact]
+        //public void Test6()
+        //{
+
+        //    //Arrange
+        //    HomeController controller = new HomeController(null, null);
+
+        //    //Act
+
+        //    ViewResult result = controller.Login() as ViewResult;
+
+
+        //    //Assert
+        //    Assert.NotNull(result);
+        //}*/
     }
+        public class transactionTest1
+        {
+        [Fact]
+        public void Test10()
+        {
 
+
+            TransactionsController controller = new TransactionsController(null, null);
+
+            ViewResult result = controller.Index() as ViewResult;
+
+            Assert.NotNull(result);
+        }
+    }
 }
+
